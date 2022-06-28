3b67b2f8f935f9b339d339f51ecf68db2756403d
TrueUp Videography <videographydiamond@gmail.com>
2022-06-28 08:02:43 +0530

    added defer on hom page

--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -26,11 +26,11 @@
     <META name="expires" content="never">
      <!--Meta Data Structure-->
      <!--googe add veryfi-->
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8673849322443357"
+     <script defer async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8673849322443357"
      crossorigin="anonymous"></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-232924169-1"></script>
+    <script  defer async src="https://www.googletagmanager.com/gtag/js?id=UA-232924169-1"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
